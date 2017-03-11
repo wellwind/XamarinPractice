3651442f82bef330daf1faa14dfe5dccbf54670a
wellwind <wellwind@mail2000.com.tw>
2017-03-11 21:24:11 +0800

    Xamarin.iOS

---
+++ iPhonePractice/iPhonePractice/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace iPhonePractice
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            btn.TouchUpInside += (sender, args) =>
+            {
+                lblOutput.Text = txtInput.Text;
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
