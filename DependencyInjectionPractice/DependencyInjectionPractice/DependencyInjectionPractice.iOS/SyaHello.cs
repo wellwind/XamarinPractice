035963ea803ea01b4c8b8f8b00d64e5ddaffc6d5
wellwind <wellwind@mail2000.com.tw>
2017-03-12 10:09:52 +0800

    DependencyInjection from different platform

---
+++ DependencyInjectionPractice/DependencyInjectionPractice/DependencyInjectionPractice.iOS/SyaHello.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DependencyInjectionPractice.iOS.SayHello))]
+namespace DependencyInjectionPractice.iOS
+{
+    public class SayHello : ISayHello
+    {
+        public string Hello()
+        {
+            return "iOS";
+        }
+    }
+}
