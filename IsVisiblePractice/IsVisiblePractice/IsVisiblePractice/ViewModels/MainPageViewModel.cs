2b781367d60dff643ba206d5955c5196dc83a277
wellwind <wellwind@mail2000.com.tw>
2017-03-12 13:49:49 +0800

    IsVisible Practice

---
+++ IsVisiblePractice/IsVisiblePractice/IsVisiblePractice/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IsVisiblePractice.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+
+        #region IsText1Visible
+        private bool _isText1Visible;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public bool IsText1Visible
+        {
+            get { return this._isText1Visible; }
+            set { this.SetProperty(ref this._isText1Visible, value); }
+        }
+        #endregion
+
+        #region IsText2Visible
+        private bool _isText2Visible;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public bool IsText2Visible
+        {
+            get { return this._isText2Visible; }
+            set { this.SetProperty(ref this._isText2Visible, value); }
+        }
+        #endregion
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+        public DelegateCommand Toggle1Command { get; set; }
+
+        public DelegateCommand Toggle2Command { get; set; }
+
+        public MainPageViewModel()
+        {
+            IsText1Visible = true;
+            IsText2Visible = true;
+
+            Toggle1Command = new DelegateCommand(() =>
+            {
+                IsText1Visible = !IsText1Visible;
+            });
+
+            Toggle2Command = new DelegateCommand(() =>
+            {
+                IsText2Visible = !IsText2Visible;
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ IsVisiblePractice/IsVisiblePractice/IsVisiblePractice/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace IsVisiblePractice.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
