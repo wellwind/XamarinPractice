57c35222bdb6f56558f80076870b83ef7b787608
wellwind <wellwind@mail2000.com.tw>
2017-03-18 17:18:18 +0800

    ListView basic binding

---
+++ ListViewPractice_01_Basic/ListViewPractice_01_Basic/ListViewPractice_01_Basic/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ListViewPractice_01_Basic.ViewModels
+{
+    public class Student
+    {
+        public string StuNum { get; set; }
+        public string StuNam { get; set; }
+    }
+
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private ObservableCollection<Student> _students;
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set { _students = value; SetProperty(ref _students, value); }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            _students = new ObservableCollection<Student>();
+            for (int i = 0; i < 100; ++i)
+            {
+                _students.Add(new Student()
+                {
+                    StuNum = i.ToString("00000"),
+                    StuNam = String.Format("Student_{0}", i)
+                });
+            }
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ ListViewPractice_01_Basic/ListViewPractice_01_Basic/ListViewPractice_01_Basic/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ListViewPractice_01_Basic.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
