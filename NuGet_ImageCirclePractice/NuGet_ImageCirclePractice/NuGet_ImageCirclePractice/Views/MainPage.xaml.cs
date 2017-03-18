4fef3e28e4c2b03a29469d4ae020cde37c66a029
wellwind <wellwind@mail2000.com.tw>
2017-03-18 16:29:18 +0800

    ImageCircle => using NuGet

---
+++ NuGet_ImageCirclePractice/NuGet_ImageCirclePractice/NuGet_ImageCirclePractice/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace NuGet_ImageCirclePractice.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
