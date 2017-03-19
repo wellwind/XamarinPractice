e7f44aa64b7b7720a09f0e8b65a504d1a786c02d
wellwind <wellwind@mail2000.com.tw>
2017-03-19 16:46:31 +0800

    ValueConverter Practice

---
+++ ValueConverterPractice/ValueConverterPractice/ValueConverterPractice/StringToColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ValueConverterPractice
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                var val = value as string;
+                switch (val)
+                {
+                    case "1":
+                        return Color.Red;
+                        break;
+                    case "2":
+                        return Color.Green;
+                        break;
+                    case "3":
+                        return Color.Blue;
+                }
+            }
+            return Color.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ValueConverterPractice/ValueConverterPractice/ValueConverterPractice/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValueConverterPractice.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+
+        #region InputColor
+        private string _inputColor;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public string InputColor
+        {
+            get { return this._inputColor; }
+            set { this.SetProperty(ref this._inputColor, value); }
+        }
+        #endregion
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
